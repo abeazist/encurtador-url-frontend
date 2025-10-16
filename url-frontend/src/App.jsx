94062abd9d3afbd77397777069c719e0ef951351
fernanda <fer.r.benetti@gmail.com>
2025-10-16 17:59:43 -0300

    teste corrigindo inversão de cores

--- url-frontend/src/App.jsx
+++ url-frontend/src/App.jsx
@@ -2,13 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TelaPrincipal from "./pages/TelaPrincipal";
 
 function App() {
-  return(
+  useEffect(() => {
+    // Força o navegador a usar o modo claro
+    document.documentElement.style.colorScheme = "light";
+    document.body.style.backgroundColor = "#fff"; // garante fundo branco
+    document.body.style.color = "#000"; // garante texto escuro
+  }, []);
+
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TelaPrincipal/>}>
-        
-        <Route path="encurtador" element={<TelaPrincipal/>} />
-          
+        <Route path="/" element={<TelaPrincipal />}>
+
+          <Route path="encurtador" element={<TelaPrincipal />} />
+
         </Route>
       </Routes>
     </BrowserRouter>
