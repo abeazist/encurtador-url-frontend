35dedd9c1bfb505e11adb5480aea73bdcad90bc0
beatriz <128046453+abeazist@users.noreply.github.com>
2025-10-16 16:17:44 -0300

    erro de importação

--- url-frontend/src/App.jsx
+++ url-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TelaPrincipal from "./pages/telaPrincipal";
+import TelaPrincipal from "./pages/TelaPrincipal";
 
 function App() {
   return(
