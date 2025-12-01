3253974cf5554c528ab413fd5b109dd3241027a5
beatriz <128046453+abeazist@users.noreply.github.com>
2025-12-01 02:02:15 -0300

    errei a URL

--- url-frontend/src/services/api.js
+++ url-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://encurtador-url-backend-l9dr.onrender.com",
 });
 
 export default api;
