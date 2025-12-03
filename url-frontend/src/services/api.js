1cec8b4a01ad911e7047079909d3eef8326c5a88
beatriz <128046453+abeazist@users.noreply.github.com>
2025-12-03 10:41:46 -0300

    update baseURL

--- url-frontend/src/services/api.js
+++ url-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://encurtador-url-backend-l9dr.onrender.com"
+  baseURL: "https://encurtador-url-backend-rn2ibrv4n-beatrizs-projects-d3f5406d.vercel.app/"
 });
 
 export default api;
